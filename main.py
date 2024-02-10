f60010efdc1e654a6e4271d78deb0eadf2b6fc6b
Alexander <alexander.kappanen@ya.ru>
2024-02-11 02:11:25 +0300

    БЕта версия

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+import json
+import logging
 import os
 import re
-import logging
-import json  # Добавили импорт json
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 
+# Импорт из plyer для работы с файловой системой
+from plyer import filechooser
+
 # Константы
-INPUT_FOLDER = "text_files_folder"
-OUTPUT_FILE = 'results.txt'
-FILES_LIST_FILE = 'spisok_failov.txt'
-SETTINGS_FILE = 'settings.json'  # Изменили расширение файла на .json
+SETTINGS_FILE = 'settings.json'
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -23,10 +26,17 @@ class ToolCheckerApp(App):
 
 class MultiLineTextInput(TextInput):
     def keyboard_on_key_down(self, window, keycode, text, modifiers):
-        if keycode[0] == 13:  # Отлавливаем нажатие Enter
-            self.insert_text('\n')  # Вставляем символ новой строки
+        if keycode[0] == 13:
+            self.insert_text('\n')
         else:
             super(MultiLineTextInput, self).keyboard_on_key_down(window, keycode, text, modifiers)
+
+class FolderChooserPopup(Popup):
+    def __init__(self, callback, **kwargs):
+        super(FolderChooserPopup, self).__init__(**kwargs)
+        self.file_chooser = FileChooserListView(path='/', dirselect=True)
+        self.callback = callback
+        self.content = self.file_chooser
 
 class ToolCheckerBoxLayout(BoxLayout):
     def __init__(self, **kwargs):
@@ -49,14 +59,29 @@ class ToolCheckerBoxLayout(BoxLayout):
         self.header_value = ""
         self.separator_value = ""
         self.footer_value = ""
+        self.selected_folder = ""
 
         # Загрузка сохраненных настроек при запуске
         self.load_settings()
 
+        # Кнопка для выбора папки
+        folder_button = Button(text='Выбрать папку', size_hint=(None, None), size=(200, 50))
+        folder_button.bind(on_release=self.open_folder_chooser)
+        self.add_widget(folder_button)
+
         # Кнопка для запуска обработки файлов
-        button = Button(text='Обработать файлы', size_hint=(None, None), size=(200, 50))
-        button.bind(on_release=self.process_files)
-        self.add_widget(button)
+        process_button = Button(text='Обработать файлы', size_hint=(None, None), size=(200, 50))
+        process_button.bind(on_release=self.process_files)
+        self.add_widget(process_button)
+
+    def open_folder_chooser(self, _):
+        # Используем plyer для выбора диска
+        folder_path = filechooser.choose_dir()
+        if folder_path:
+            self.set_selected_folder(folder_path[0])
+
+    def set_selected_folder(self, selected_folder):
+        self.selected_folder = selected_folder
 
     def process_files(self, _):
         """Обработка файлов по заданным параметрам."""
@@ -69,26 +94,22 @@ class ToolCheckerBoxLayout(BoxLayout):
         self.save_settings()
 
         self.read_and_copy_files(
-            INPUT_FOLDER,
+            self.selected_folder,
             self.header_value,
             self.separator_value,
-            self.footer_value,
-            OUTPUT_FILE,
-            FILES_LIST_FILE
+            self.footer_value
         )
         logger.info('Файлы обработаны!\n')
 
     def read_and_copy_files(
-            self, input_folder, header, separator, footer, output_file, files_list_file):
+            self, input_folder, header, separator, footer):
         unique_texts = set()
-        processed_files = []
 
         def process_folder(folder):
-            for root, _, files in os.walk(folder):
+            for root, dirs, files in os.walk(folder):
                 for file_name in files:
                     file_path = os.path.join(root, file_name)
                     if os.path.isfile(file_path) and file_name.lower().endswith('.mpf'):
-                        processed_files.append(file_name)
                         with open(file_path, 'r', encoding='utf-8') as file:
                             lines = file.readlines()
                             for line in lines:
@@ -97,10 +118,7 @@ class ToolCheckerBoxLayout(BoxLayout):
                                     unique_texts.update(match for match in matches)
 
         process_folder(input_folder)
-        logger.info("Обработанные файлы: %s", processed_files)
-
-        with open(files_list_file, 'w', encoding='utf-8') as file:
-            file.write('\n'.join(processed_files))
+        logger.info("Обработанные файлы в папке: %s", input_folder)
 
         result = [header]
         if unique_texts:
@@ -110,10 +128,10 @@ class ToolCheckerBoxLayout(BoxLayout):
 
         result.append(footer)
 
+        output_file = os.path.join(input_folder, 'ToolChecker.mpf')
         with open(output_file, 'w', encoding='utf-8') as file:
             file.write('\n'.join(result))
 
-        processed_files.clear()
         unique_texts.clear()
 
     def save_settings(self):
@@ -121,7 +139,8 @@ class ToolCheckerBoxLayout(BoxLayout):
         settings = {
             "Header": self.header_value,
             "Separator": self.separator_value,
-            "Footer": self.footer_value
+            "Footer": self.footer_value,
+            "SelectedFolder": self.selected_folder
         }
 
         with open(SETTINGS_FILE, 'w', encoding='utf-8') as file:
@@ -135,6 +154,7 @@ class ToolCheckerBoxLayout(BoxLayout):
                 self.header_input.text = settings.get("Header", "")
                 self.separator_input.text = settings.get("Separator", "")
                 self.footer_input.text = settings.get("Footer", "")
+                self.selected_folder = settings.get("SelectedFolder", "")
 
 if __name__ == '__main__':
     ToolCheckerApp().run()
