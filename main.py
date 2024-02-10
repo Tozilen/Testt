efe149cf3daec4ab06fda0912a43e162884dd55d
Alexander <alexander.kappanen@ya.ru>
2024-02-11 00:09:25 +0300

    Готовая внутрения логика

--- main.py
+++ main.py
@@ -1,87 +1,108 @@
 import os
 import re
+import logging
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
+# Constants
+INPUT_FOLDER = "text_files_folder"
+HEADER_FILE = 'header.txt'
+SEPARATOR_FILE = 'separator.txt'
+FOOTER_FILE = 'footer.txt'
+OUTPUT_FILE = 'results.txt'
+FILES_LIST_FILE = 'spisok_failov.txt'
 
-class MyBoxLayout(BoxLayout):
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class ToolCheckerApp(App):
+    def build(self):
+        return ToolCheckerBoxLayout()
+
+class ToolCheckerBoxLayout(BoxLayout):
     def __init__(self, **kwargs):
-        super(MyBoxLayout, self).__init__(**kwargs)
+        super(ToolCheckerBoxLayout, self).__init__(**kwargs)
         self.orientation = 'vertical'
 
-        # Добавьте кнопку для запуска обработки файлов
         button = Button(text='Обработать файлы', size_hint=(None, None), size=(200, 50))
         button.bind(on_release=self.process_files)
         self.add_widget(button)
 
     def process_files(self, _):
-        input_folder = "text_files_folder"  # папка с текстовыми файлами
-        header_file = 'header.txt'  # файл со шапкой
-        separator_file = 'separator.txt'  # файл с разделителем
-        footer_file = 'footer.txt'  # файл с концом
-        output_file = 'results.txt'  # файл для сохранения результатов
-        files_list_file = 'spisok_failov.txt'  # файл для сохранения списка обработанных файлов
-
-        # Вызываем функцию для обработки файлов
+        """Обработка файлов по заданным параметрам."""
         self.read_and_copy_files(
-            input_folder, header_file, separator_file, footer_file, output_file, files_list_file)
-
-        print('Файлы обработаны!')
+            INPUT_FOLDER, HEADER_FILE, SEPARATOR_FILE, FOOTER_FILE, OUTPUT_FILE, FILES_LIST_FILE
+        )
+        logger.info('Файлы обработаны!\n')
 
     def read_and_copy_files(
             self, input_folder, header_file, separator_file, footer_file, output_file, files_list_file):
-        texts = []
-        processed_files = []  # Новый список для хранения названий обработанных файлов
+        """
+        Чтение и копирование файлов с учетом заданных параметров.
 
-        # Функция для рекурсивного обхода файлов вложенных папок
+        :param input_folder: Папка с входными файлами.
+        :param header_file: Путь к файлу с заголовком.
+        :param separator_file: Путь к файлу с разделителем.
+        :param footer_file: Путь к файлу с футером.
+        :param output_file: Путь к файлу с результатами.
+        :param files_list_file: Путь к файлу со списком обработанных файлов.
+        """
+        unique_texts = set()  # Множество для уникальных текстов
+        processed_files = []  # Список для обработанных файлов
+
         def process_folder(folder):
-            for root, _dirs, files in os.walk(folder):
+            for root, _, files in os.walk(folder):
                 for file_name in files:
                     file_path = os.path.join(root, file_name)
                     if os.path.isfile(file_path) and file_name.lower().endswith('.mpf'):
-                        processed_files.append(file_name)  # Добавляем название файла в список
+                        processed_files.append(file_name)
                         with open(file_path, 'r', encoding='utf-8') as file:
-                            # Используем регулярное выражение для поиска строк, начинающихся с 'T='
-                            matches = re.findall(r'T=.*', file.read())
-                            # Добавляем найденные строки в список texts
-                            texts.extend(matches)
+                            # Чтение строк из файла
+                            lines = file.readlines()
 
-        # Вызываем функцию для обработки файлов
+                            # Ищем тексты в формате T="текст" в строках, отсеянных от комментариев
+                            for line in lines:
+                                # Отсев строк, начинающихся с ';'
+                                if not line.lstrip().startswith(';'):
+                                    matches = re.findall(r'T="([^"]+)"', line, flags=re.DOTALL)
+                                    unique_texts.update(match for match in matches)
+
         process_folder(input_folder)
-        print("Обработанные файлы:", processed_files)  # Выводим список обработанных файлов
+        logger.info("Обработанные файлы: %s", processed_files)
 
-        # Сохранение списка обработанных файлов в файле
+        # Запись списка обработанных файлов
         with open(files_list_file, 'w', encoding='utf-8') as file:
             file.write('\n'.join(processed_files))
 
-        # Чтение файлов с шапкой, разделителем и концом
+        # Чтение заголовка
         with open(header_file, 'r', encoding='utf-8') as file:
             header = file.read().strip()
 
+        # Чтение разделителя
         with open(separator_file, 'r', encoding='utf-8') as file:
             separator = file.read().strip()
 
+        # Чтение футера
         with open(footer_file, 'r', encoding='utf-8') as file:
             footer = file.read().strip()
 
-        # Формирование списка и запись его в файл результатов
         result = [header]
-        if texts:
-            for i, text in enumerate(texts, start=1):
-                result.append(f'N{i} {text}')
+        if unique_texts:
+            for i, text in enumerate(unique_texts, start=1):
+                result.append(f'N{i} T="{text}"')
                 result.append(separator)
-            result.pop()  # Убираем последний разделитель
+
         result.append(footer)
 
+        # Запись результатов в файл
         with open(output_file, 'w', encoding='utf-8') as file:
             file.write('\n'.join(result))
 
-
-class MyApp(App):
-    def build(self):
-        return MyBoxLayout()
-
+        # Очистка списков после выполнения программы
+        processed_files.clear()
+        unique_texts.clear()
 
 if __name__ == '__main__':
-    MyApp().run()
+    ToolCheckerApp().run()
